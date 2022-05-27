4d0560a7e4e24e972b77f49daaddd32e63d690d2
SilaghiAlex <74719905+SilaghiAlex@users.noreply.github.com>
2022-05-28 00:00:23 +0300

    proiect

--- Service/Service/Program.cs
+++ Service/Service/Program.cs
@@ -1,44 +1,31 @@
-﻿using CommandLine;
-using Mapster.Service.Endpoints;
+using System.Buffers.Binary;
+using System.IO.Compression;
 
-namespace Mapster.Service;
-
-public class Options
+// The primary namespace of the application
+namespace Mapster
 {
-    [Option('i', "input", Required = true, HelpText = "Path to map data binary file")]
-    public string? MapDataPath { get; set; }
-}
-
-internal static class Program
-{
-    // Set up Main as an async method
-    private static async Task Main(string[] args)
+    class Program
     {
-        Options? arguments = null;
-        var argParseResult = Parser.Default.ParseArguments<Options>(args).WithParsed(options =>
+        static void Main(string[] args)
         {
-            arguments = options;
-        });
+            using FileStream fs = new FileStream(@"", FileMode.Open);
 
-        if (argParseResult.Errors.Any())
-        {
-            Environment.Exit(-1);
+            byte[] buffer = new byte[4];
+            fs.Read(buffer, 0, buffer.Length);
+            int headerSize = BinaryPrimitives.ReadInt32BigEndian(buffer.AsSpan());
+
+            byte[] headerBuffer = new byte[headerSize];
+            fs.Read(headerBuffer, 0, headerBuffer.Length);
+            BlobHeader blobHeader = BlobHeader.Parser.ParseFrom(headerBuffer);
+
+            byte[] blobBuffer = new byte[blobHeader.Datasize];
+            fs.Read(blobBuffer, 0, blobBuffer.Length);
+            Blob blob = Blob.Parser.ParseFrom(blobBuffer);
+
+            var compressedData = blob.ZlibData.Span;
+            MemoryStream ms = new MemoryStream(compressedData.ToArray());
+            ZLibStream zlibStream = new ZLibStream(ms, CompressionMode.Decompress);
+            HeaderBlock headerBlock = HeaderBlock.Parser.ParseFrom(zlibStream);
         }
-
-        // Set up a builder to help register our endpoint handlers
-        var appBuilder = WebApplication.CreateBuilder();
-
-        // Register TileEndpoint as a singleton instance but instantiate it explicitly to force data loading
-        var tileEndpoint = new TileEndpoint(arguments!.MapDataPath!);
-        appBuilder.Services.AddSingleton(_ => tileEndpoint);
-
-        // Create the application instance
-        var app = appBuilder.Build();
-        // Set up to serve on localhost's unpriviledged 8080 port
-        app.Urls.Add("http://localhost:8080");
-
-        TileEndpoint.Register(app);
-
-        await app.RunAsync();
     }
 }
